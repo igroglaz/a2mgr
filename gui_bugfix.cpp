9fba892289d1797f02661b846873d5336b4a979b
igroglaz <39904162+igroglaz@users.noreply.github.com>
2022-08-22 13:25:08 +0300

    format & encoding

--- gui_bugfix.cpp
+++ gui_bugfix.cpp
@@ -7,142 +7,149 @@
 using namespace zxmgr;
 using namespace std;
 
-// Определение позиции курсора в тексте из координат клика на поле
-// fix: более точное соответствие координат позиции курсора
-// fix: исправлен курсор в запароленном поле
-// fix: исправлено определение позиции в случае с нестандартным расположением текстового поля (X!=0)
+// РћРїСЂРµРґРµР»РµРЅРёРµ РїРѕР·РёС†РёРё РєСѓСЂСЃРѕСЂР° РІ С‚РµРєСЃС‚Рµ РёР· РєРѕРѕСЂРґРёРЅР°С‚ РєР»РёРєР° РЅР° РїРѕР»Рµ
+// fix: Р±РѕР»РµРµ С‚РѕС‡РЅРѕРµ СЃРѕРѕС‚РІРµС‚СЃС‚РІРёРµ РєРѕРѕСЂРґРёРЅР°С‚ РїРѕР·РёС†РёРё РєСѓСЂСЃРѕСЂР°
+// fix: РёСЃРїСЂР°РІР»РµРЅ РєСѓСЂСЃРѕСЂ РІ Р·Р°РїР°СЂРѕР»РµРЅРЅРѕРј РїРѕР»Рµ
+// fix: РёСЃРїСЂР°РІР»РµРЅРѕ РѕРїСЂРµРґРµР»РµРЅРёРµ РїРѕР·РёС†РёРё РІ СЃР»СѓС‡Р°Рµ СЃ РЅРµСЃС‚Р°РЅРґР°СЂС‚РЅС‹Рј СЂР°СЃРїРѕР»РѕР¶РµРЅРёРµРј С‚РµРєСЃС‚РѕРІРѕРіРѕ РїРѕР»СЏ (X!=0)
 unsigned long _stdcall GUI_textField_MeasurePosition(unsigned long x)
 {
-	unsigned long pthis;
-	__asm mov pthis, ecx;
+    unsigned long pthis;
+    __asm mov pthis, ecx;
 
-	unsigned long pos = 0;
-	bool fpos = false;
+    unsigned long pos = 0;
+    bool fpos = false;
 
-	char* ctx = *(char**)(pthis+0x5C);
+    char* ctx = *(char**)(pthis+0x5C);
 
-	string tx = "";
+    string tx = "";
 
-	if(*(unsigned long*)(pthis+0x7C)) // password field
-	{
-		for(int i = 0; i < strlen(ctx); i++)
-			ctx[i] = '*';
-	}
+    if (*(unsigned long*)(pthis+0x7C)) // password field
+    {
+        for (int i = 0; i < strlen(ctx); i++)
+            ctx[i] = '*';
+    }
 
-	for(int i = 0; i < strlen(ctx); i++)
-	{
-		tx += ctx[i];
-		int wd = Font::MeasureTextWidth(*(unsigned long*)(pthis+0x60), tx.c_str());
-		RECT* clrec = new RECT;
-		GUI::GetClientRect(pthis, clrec);
-		int innerpos = x-clrec->left;
-		if(wd >= innerpos)
-		{
-			char thissym[2] = {ctx[i], 0};
-			int wdx = (Font::MeasureTextWidth(*(unsigned long*)(pthis+0x60), thissym)-4)/2;
-			if(wd-wdx >= innerpos) pos = i;
-			else pos = i+1;
-			fpos = true;
+    for (int i = 0; i < strlen(ctx); i++)
+    {
+        tx += ctx[i];
+        int wd = Font::MeasureTextWidth(*(unsigned long*)(pthis+0x60), tx.c_str());
+        RECT* clrec = new RECT;
+        GUI::GetClientRect(pthis, clrec);
+        int innerpos = x-clrec->left;
 
-			break;
-		}
-	}
+        if (wd >= innerpos)
+        {
+            char thissym[2] = {ctx[i], 0};
+            int wdx = (Font::MeasureTextWidth(*(unsigned long*)(pthis+0x60), thissym)-4)/2;
 
-	if(!fpos) pos = strlen(ctx);
+            if (wd-wdx >= innerpos)
+                pos = i;
+            else
+                pos = i+1;
 
-	return pos;
+            fpos = true;
+
+            break;
+        }
+    }
+
+    if (!fpos) pos = strlen(ctx);
+
+    return pos;
 }
 
 unsigned long OnGMEnter(unsigned long pthis, unsigned long arg0)
 {
-	unsigned long pthis_2 = 0;
-	unsigned long pthis_3 = 0;
-	unsigned long pthis_4 = 0;
+    unsigned long pthis_2 = 0;
+    unsigned long pthis_3 = 0;
+    unsigned long pthis_4 = 0;
 
-	__asm
-	{
-		mov		edx, 0x00401870
-		call	edx
-		mov		[pthis_2], eax
+    __asm
+    {
+        mov     edx, 0x00401870
+        call    edx
+        mov     [pthis_2], eax
 
-		mov		ecx, [pthis_2]
-		add		ecx, 0x468
-		mov		[pthis_4], ecx
+        mov     ecx, [pthis_2]
+        add     ecx, 0x468
+        mov     [pthis_4], ecx
 
-		
-	}
+        
+    }
 
-	unsigned long p_id1 = *(unsigned long*)(pthis_4 + 0x08);
-	unsigned long p_id2 = *(unsigned long*)(pthis_4 + 0x0C);
+    unsigned long p_id1 = *(unsigned long*)(pthis_4 + 0x08);
+    unsigned long p_id2 = *(unsigned long*)(pthis_4 + 0x0C);
 
-	if((p_id2 & 0x3F000000) == 0x3F000000) return 1;
-	return 0;
+    if ((p_id2 & 0x3F000000) == 0x3F000000)
+        return 1;
+
+    return 0;
 }
 
 void __declspec(naked) GUI_gmEnter()
 { // 44DB8D
-	__asm
-	{
-		push	ebp
-		mov		ebp, esp
-		push	0xFFFFFFFF
-		push	0x005C70DC
-		mov		eax, fs:[0]
-		push	eax
-		mov		fs:[0], esp
-		sub		esp, 0x34
-		mov		[ebp-0x2C], ecx
+    __asm
+    {
+        push    ebp
+        mov     ebp, esp
+        push    0xFFFFFFFF
+        push    0x005C70DC
+        mov     eax, fs:[0]
+        push    eax
+        mov     fs:[0], esp
+        sub     esp, 0x34
+        mov     [ebp-0x2C], ecx
 
-		push	[ebp+0x08]
-		push	ecx
-		call	OnGMEnter
-		add		esp, 8
-		test	eax, eax
-		jnz		ggme_ret1
+        push    [ebp+0x08]
+        push    ecx
+        call    OnGMEnter
+        add     esp, 8
+        test    eax, eax
+        jnz     ggme_ret1
 
-		mov		edx, 0x0044DBAB
-		jmp		edx
+        mov     edx, 0x0044DBAB
+        jmp     edx
 
 ggme_ret1:
-		mov		eax, 1
-		mov		ecx, [ebp-0x0C]
-		mov		fs:[0], ecx
-		mov		esp, ebp
-		pop		ebp
-		retn	0x0004
-		
-	}
+        mov     eax, 1
+        mov     ecx, [ebp-0x0C]
+        mov     fs:[0], ecx
+        mov     esp, ebp
+        pop     ebp
+        retn    0x0004
+        
+    }
 }
 
 void __declspec(naked) GUI_softcoreEnter()
 {
-	__asm
-	{ // 44DD0F
-		mov		ecx, [ebp-0x18]
-		add		ecx, 0x468
+    __asm
+    { // 44DD0F
+        mov     ecx, [ebp-0x18]
+        add     ecx, 0x468
 
-		mov		edx, [ebp-0x10]
-		add		edx, 1
+        mov     edx, [ebp-0x10]
+        add     edx, 1
 
-		cmp		[z_softcore], 0
-		jnz		test_upper
+        cmp     [z_softcore], 0
+        jnz     test_upper
 
-		cmp		[ecx+0x114], edx
-		jg		test_failed
+        cmp     [ecx+0x114], edx
+        jg      test_failed
 
 test_upper:
-		cmp		[ecx+0x118], edx
-		jl		test_failed
+        cmp     [ecx+0x118], edx
+        jl      test_failed
 
-		cmp		[ebp-0x14], 0x10
-		jge		test_failed
+        cmp     [ebp-0x14], 0x10
+        jge     test_failed
 
-		mov		[ebp-0x40], 1
-		mov		edx, 0x0044DD55
-		jmp		edx
+        mov     [ebp-0x40], 1
+        mov     edx, 0x0044DD55
+        jmp     edx
 
 test_failed:
-		mov		edx, 0x0044DD4E
-		jmp		edx
-	}
+        mov     edx, 0x0044DD4E
+        jmp     edx
+    }
 }
--- network_protocol.cpp
+++ network_protocol.cpp
@@ -20,202 +20,202 @@ unsigned long _neI_SessionKey = 0, _neI_SessionID = 0;
 
 void _stdcall crcmain(char* data)
 {
-	// data:0 = allods2.exe ^ key
-	// data:4 = a2mgr.dll ^ key + allods2.exe
-	// data:8 = patch.res ^ a2mgr.dll
-	// data:C = world.res ^ key - patch.res
-	// data:10 = graphics.res + allods2.exe ^ key (nulled/unused)
-	// data:14 = key
-	// data:18 = UUID
-	// data:2C = UUID CRC32
+    // data:0 = allods2.exe ^ key
+    // data:4 = a2mgr.dll ^ key + allods2.exe
+    // data:8 = patch.res ^ a2mgr.dll
+    // data:C = world.res ^ key - patch.res
+    // data:10 = graphics.res + allods2.exe ^ key (nulled/unused)
+    // data:14 = key
+    // data:18 = UUID
+    // data:2C = UUID CRC32
 
-	net_key = 0x6CDB248D;
-	//net_key = _neI_SessionKey;
+    net_key = 0x6CDB248D;
+    //net_key = _neI_SessionKey;
 
-	CRC_32 crc;
-	crc32_allods2_exe = crc.CalcCRC(sha1_allods2_exe, 20);
-	crc32_a2mgr_dll = crc.CalcCRC(sha1_a2mgr_dll, 20);
-	crc32_patch_res = crc.CalcCRC(sha1_patch_res, 20);
-	crc32_world_res = crc.CalcCRC(sha1_world_res, 20);
-	//crc32_graphics_res = crc.CalcCRC(sha1_graphics_res, 20);
+    CRC_32 crc;
+    crc32_allods2_exe = crc.CalcCRC(sha1_allods2_exe, 20);
+    crc32_a2mgr_dll = crc.CalcCRC(sha1_a2mgr_dll, 20);
+    crc32_patch_res = crc.CalcCRC(sha1_patch_res, 20);
+    crc32_world_res = crc.CalcCRC(sha1_world_res, 20);
+    //crc32_graphics_res = crc.CalcCRC(sha1_graphics_res, 20);
 
-	unsigned long lrc_allods2_exe = crc32_allods2_exe ^ net_key;
-	unsigned long lrc_a2mgr_dll = (crc32_a2mgr_dll ^ net_key) + crc32_allods2_exe;
-	unsigned long lrc_patch_res = crc32_patch_res ^ crc32_a2mgr_dll;
-	unsigned long lrc_world_res = (crc32_world_res ^ net_key) - crc32_patch_res;
-	//unsigned long lrc_graphics_res = (crc32_graphics_res + crc32_allods2_exe) ^ net_key;
+    unsigned long lrc_allods2_exe = crc32_allods2_exe ^ net_key;
+    unsigned long lrc_a2mgr_dll = (crc32_a2mgr_dll ^ net_key) + crc32_allods2_exe;
+    unsigned long lrc_patch_res = crc32_patch_res ^ crc32_a2mgr_dll;
+    unsigned long lrc_world_res = (crc32_world_res ^ net_key) - crc32_patch_res;
+    //unsigned long lrc_graphics_res = (crc32_graphics_res + crc32_allods2_exe) ^ net_key;
 
-	memset(data, 0, 0x18);
-	*(unsigned long*)(data) = lrc_allods2_exe;
-	*(unsigned long*)(data + 4) = lrc_a2mgr_dll;
-	*(unsigned long*)(data + 8) = lrc_patch_res;
-	*(unsigned long*)(data + 0x0C) = lrc_world_res;
-	*(unsigned long*)(data + 0x10) = net_key;//lrc_graphics_res;
-	*(unsigned long*)(data + 0x14) = _neI_SessionID ^ 0xDEADBEEF;
-	memcpy(data + 0x18, sha1_uuid, 20);
+    memset(data, 0, 0x18);
+    *(unsigned long*)(data) = lrc_allods2_exe;
+    *(unsigned long*)(data + 4) = lrc_a2mgr_dll;
+    *(unsigned long*)(data + 8) = lrc_patch_res;
+    *(unsigned long*)(data + 0x0C) = lrc_world_res;
+    *(unsigned long*)(data + 0x10) = net_key;//lrc_graphics_res;
+    *(unsigned long*)(data + 0x14) = _neI_SessionID ^ 0xDEADBEEF;
+    memcpy(data + 0x18, sha1_uuid, 20);
 
-	*(unsigned long*)(data + 0x2C) = crc.CalcCRC((LPVOID)(data + 0x18), 20) ^ net_key;
-	for(int i = 0; i < 5; i++)
-		*(unsigned long*)(data + 0x18 + i * 4) ^= net_key;
+    *(unsigned long*)(data + 0x2C) = crc.CalcCRC((LPVOID)(data + 0x18), 20) ^ net_key;
+    for (int i = 0; i < 5; i++)
+        *(unsigned long*)(data + 0x18 + i * 4) ^= net_key;
 
-	unsigned long uoth = *(unsigned long*)(data + 0x40);
-	unsigned long gamemode = (uoth & 0x00FF0000) >> 16;
+    unsigned long uoth = *(unsigned long*)(data + 0x40);
+    unsigned long gamemode = (uoth & 0x00FF0000) >> 16;
 
-	if(gamemode == 4)
-	{
-		unsigned long mainWnd = zxmgr::AfxGetMainWnd();
-		*(unsigned long*)(mainWnd + 0x3C8) = 0; // reset gamemode to 0
-		z_softcore = 2;
-	}
-	else if(gamemode == 2)
-	{
-		unsigned long mainWnd = zxmgr::AfxGetMainWnd();
-		*(unsigned long*)(mainWnd + 0x3C8) = 0; // reset gamemode to 0
-		z_softcore = 1;
-	}
-	else
-	{
-		z_softcore = 0;
-	}
+    if (gamemode == 4) // GAMEMODE_SANDBOX
+    {
+        unsigned long mainWnd = zxmgr::AfxGetMainWnd();
+        *(unsigned long*)(mainWnd + 0x3C8) = 0; // reset gamemode to 0
+        z_softcore = 2;
+    }
+    else if (gamemode == 2) // GAMEMODE_SOFTCORE
+    {
+        unsigned long mainWnd = zxmgr::AfxGetMainWnd();
+        *(unsigned long*)(mainWnd + 0x3C8) = 0; // reset gamemode to 0
+        z_softcore = 1;
+    }
+    else
+    {
+        z_softcore = 0;
+    }
 }
 
 int NETPROTO_sendAuthorizationPacket() 
-/* окончательное формирование пакета авторизации на хете и отправка */
+/* РѕРєРѕРЅС‡Р°С‚РµР»СЊРЅРѕРµ С„РѕСЂРјРёСЂРѕРІР°РЅРёРµ РїР°РєРµС‚Р° Р°РІС‚РѕСЂРёР·Р°С†РёРё РЅР° С…РµС‚Рµ Рё РѕС‚РїСЂР°РІРєР° */
 { // calc size & send
-	_asm {
-		push	ebp
-		mov	ebp, esp
-		push	ecx
-		mov	[ebp-0x04], ecx
-		mov	ecx, [ebp-0x04]
-		mov	eax, ecx
-		cmp	byte ptr [ecx+9], 0
-		jnz	norm_p // не пакет авторизации, обработать по человечески
-		// обработать с доп. размером
-		add	eax, 50h 
-		push	eax		// char *
-		mov	edx, 0x58D7B0
-		call	edx
-		add	esp, 4
-		mov	ecx, [ebp-0x04]
-		mov	[ecx+4Eh], ax
-		mov	edx, [ebp-0x04]
-		xor	eax, eax
-		mov	ax, [edx+4Eh]
-		add	eax, 0x48
-		jmp	join
-norm_p:		add	eax, 10h 
-		push	eax		// char *
-		mov	edx, 0x58D7B0
-		call	edx
-		add	esp, 4
-		mov	ecx, [ebp-0x04]
-		mov	[ecx+0Eh], ax
-		mov	edx, [ebp-0x04]
-		xor	eax, eax
-		mov	ax, [edx+0Eh]
-		add	eax, 0x8
-join:		push	eax
-		mov	ecx, [ebp-0x04]
-		add	ecx, 9
-		push	ecx
-		mov	ecx, [ebp+0x08]
-		mov	edx, 0x508C6A
-		call	edx
-		mov	esp, ebp
-		pop	ebp
-		retn	4
-	}
+    _asm {
+        push    ebp
+        mov     ebp, esp
+        push    ecx
+        mov     [ebp-0x04], ecx
+        mov     ecx, [ebp-0x04]
+        mov     eax, ecx
+        cmp     byte ptr [ecx+9], 0
+        jnz     norm_p // РЅРµ РїР°РєРµС‚ Р°РІС‚РѕСЂРёР·Р°С†РёРё, РѕР±СЂР°Р±РѕС‚Р°С‚СЊ РїРѕ С‡РµР»РѕРІРµС‡РµСЃРєРё
+        // РѕР±СЂР°Р±РѕС‚Р°С‚СЊ СЃ РґРѕРї. СЂР°Р·РјРµСЂРѕРј
+        add     eax, 50h 
+        push    eax        // char *
+        mov     edx, 0x58D7B0
+        call    edx
+        add     esp, 4
+        mov     ecx, [ebp-0x04]
+        mov     [ecx+4Eh], ax
+        mov     edx, [ebp-0x04]
+        xor     eax, eax
+        mov     ax, [edx+4Eh]
+        add     eax, 0x48
+        jmp     join
+norm_p:      add    eax, 10h 
+        push    eax        // char *
+        mov     edx, 0x58D7B0
+        call    edx
+        add     esp, 4
+        mov     ecx, [ebp-0x04]
+        mov     [ecx+0Eh], ax
+        mov     edx, [ebp-0x04]
+        xor     eax, eax
+        mov     ax, [edx+0Eh]
+        add     eax, 0x8
+join:        push    eax
+        mov     ecx, [ebp-0x04]
+        add     ecx, 9
+        push    ecx
+        mov     ecx, [ebp+0x08]
+        mov     edx, 0x508C6A
+        call    edx
+        mov     esp, ebp
+        pop     ebp
+        retn    4
+    }
 }
 
 #pragma warning (disable: 4733)
 int NETPROTO_createAuthorizationPacket()
-/* подготовка пакета авторизации к отправке */
+/* РїРѕРґРіРѕС‚РѕРІРєР° РїР°РєРµС‚Р° Р°РІС‚РѕСЂРёР·Р°С†РёРё Рє РѕС‚РїСЂР°РІРєРµ */
 { // fill packet
-	_asm {
-		push	ebp
-		mov	ebp, esp
-		push	0FFFFFFFFh
-		push	0x5CF28A
-		mov	eax, fs:0
-		push	eax
-		mov	fs:0, esp
-		sub	esp, 14h
-		mov	[ebp-0x018], ecx
-		mov	[ebp-0x04], 1
-		mov	dword ptr [ebp-0x010], 0x699FC8
-		lea	eax, [ebp+0x0C]
-		push	eax
-		lea	ecx, [ebp+0x08]
-		push	ecx
-		lea	edx, [ebp-0x014]
-		push	edx
-		mov	edx, 0x5ABA87
-		call	edx
-		mov	[ebp-0x01C], eax
-		mov	eax, [ebp-0x01C]
-		mov	[ebp-0x020], eax
-		mov	byte ptr [ebp-0x04], 2
-		mov	ecx, [ebp-0x020]
-		mov	edx, 0x401860
-		call	edx
-		push	eax		// char *
-		mov	ecx, [ebp-0x010]
-		add	ecx, 50h
-		push	ecx		// char *
-		mov	edx, 0x58CDF0
-		call	edx
-		add	esp, 8
-		mov	byte ptr [ebp-0x04], 1
-		lea	ecx, [ebp-0x014]
-		mov	edx, 0x5AB84A
-		call	edx
-		mov	edx, [ebp-0x010]
-		mov	byte ptr [edx+9], 0 /* заголовок пакета */
-		//////// заполнить 0x30 байт
-		//crc
-		//lea eax, patchsign
-		//push eax
-		//lea eax, [edx+0x0A]
-		//push eax
-		//call crcmain
-		//crc
-		lea	ecx, [ebp+0x08]
-		mov	edx, 0x402970
-		call	edx
-		mov	ecx, [ebp+0x010]
-		shl	ecx, 10h
-		or	eax, ecx
-		mov	ecx, PROTO_VER
-		shl	ecx, 24
-		or	eax, ecx /* версия 11 */
-		mov	edx, [ebp-0x010]
-		mov	[edx+0x4A], eax
-		mov	edx, [ebp-0x010]
-		lea		eax, [edx+0x0A]
-		push	eax
-		call	crcmain
-		mov	eax, [ebp-0x010]
-		mov	word ptr [eax+7], 0
-		mov	ecx, [ebp-0x010]
-		push	ecx
-		mov	ecx, [ebp-0x018]
-		mov	edx, 0x50A218
-		call	edx
-		mov	byte ptr [ebp-0x04], 0
-		lea	ecx, [ebp+0x08]
-		mov	edx, 0x5AB84A
-		call	edx
-		mov	[ebp-0x04], 0FFFFFFFFh
-		lea	ecx, [ebp+0x0C]
-		mov	edx, 0x5AB84A
-		call	edx
-		mov	ecx, [ebp-0x0C]
-		mov	fs:0, ecx
-		mov	esp, ebp
-		pop	ebp
-		retn	0Ch
-	}
+    _asm {
+        push    ebp
+        mov     ebp, esp
+        push    0FFFFFFFFh
+        push    0x5CF28A
+        mov     eax, fs:0
+        push    eax
+        mov     fs:0, esp
+        sub     esp, 14h
+        mov     [ebp-0x018], ecx
+        mov     [ebp-0x04], 1
+        mov     dword ptr [ebp-0x010], 0x699FC8
+        lea     eax, [ebp+0x0C]
+        push    eax
+        lea     ecx, [ebp+0x08]
+        push    ecx
+        lea     edx, [ebp-0x014]
+        push    edx
+        mov     edx, 0x5ABA87
+        call    edx
+        mov     [ebp-0x01C], eax
+        mov     eax, [ebp-0x01C]
+        mov     [ebp-0x020], eax
+        mov     byte ptr [ebp-0x04], 2
+        mov     ecx, [ebp-0x020]
+        mov     edx, 0x401860
+        call    edx
+        push    eax        // char *
+        mov     ecx, [ebp-0x010]
+        add     ecx, 50h
+        push    ecx        // char *
+        mov     edx, 0x58CDF0
+        call    edx
+        add     esp, 8
+        mov     byte ptr [ebp-0x04], 1
+        lea     ecx, [ebp-0x014]
+        mov     edx, 0x5AB84A
+        call    edx
+        mov     edx, [ebp-0x010]
+        mov     byte ptr [edx+9], 0 /* Р·Р°РіРѕР»РѕРІРѕРє РїР°РєРµС‚Р° */
+        //////// Р·Р°РїРѕР»РЅРёС‚СЊ 0x30 Р±Р°Р№С‚
+        //crc
+        //lea eax, patchsign
+        //push eax
+        //lea eax, [edx+0x0A]
+        //push eax
+        //call crcmain
+        //crc
+        lea     ecx, [ebp+0x08]
+        mov     edx, 0x402970
+        call    edx
+        mov     ecx, [ebp+0x010]
+        shl     ecx, 10h
+        or      eax, ecx
+        mov     ecx, PROTO_VER
+        shl     ecx, 24
+        or      eax, ecx /* РІРµСЂСЃРёСЏ 11 */
+        mov     edx, [ebp-0x010]
+        mov     [edx+0x4A], eax
+        mov     edx, [ebp-0x010]
+        lea     eax, [edx+0x0A]
+        push    eax
+        call    crcmain
+        mov     eax, [ebp-0x010]
+        mov     word ptr [eax+7], 0
+        mov     ecx, [ebp-0x010]
+        push    ecx
+        mov     ecx, [ebp-0x018]
+        mov     edx, 0x50A218
+        call    edx
+        mov     byte ptr [ebp-0x04], 0
+        lea     ecx, [ebp+0x08]
+        mov     edx, 0x5AB84A
+        call    edx
+        mov     [ebp-0x04], 0FFFFFFFFh
+        lea     ecx, [ebp+0x0C]
+        mov     edx, 0x5AB84A
+        call    edx
+        mov     ecx, [ebp-0x0C]
+        mov     fs:0, ecx
+        mov     esp, ebp
+        pop     ebp
+        retn    0Ch
+    }
 }
 
 bool recv_lastCustom = false;
@@ -224,109 +224,109 @@ int recv_lastRemaining = -1;
 
 int PASCAL recv_(SOCKET s, char* buf, int len, int flags)
 {
-	if(recv_lastIncomplete)
-	{
-		int r_ = recv(s, buf, recv_lastRemaining, 0);
-		if(r_ == recv_lastRemaining)
-		{
-			recv_lastIncomplete = false;
-			recv_lastRemaining = -1;
-		}
-		
-		return r_;
-	}
+    if (recv_lastIncomplete)
+    {
+        int r_ = recv(s, buf, recv_lastRemaining, 0);
+        if (r_ == recv_lastRemaining)
+        {
+            recv_lastIncomplete = false;
+            recv_lastRemaining = -1;
+        }
+        
+        return r_;
+    }
 
-	int r = recv(s, buf, 4, 0);
-	if(r != 4)
-	{
-		//log_format("received incomplete packet (oct1 = %d).\n", r);
-		return -1;
-	}
-	if(!*(int*)(buf))
-	{
-		//log_format("received null packet.\n");
-		return 0;
-	}
+    int r = recv(s, buf, 4, 0);
+    if (r != 4)
+    {
+        //log_format("received incomplete packet (oct1 = %d).\n", r);
+        return -1;
+    }
+    if (!*(int*)(buf))
+    {
+        //log_format("received null packet.\n");
+        return 0;
+    }
 
-	r = recv(s, buf+4, 4, 0);
-	if(r != 4)
-	{
-		//log_format("received incomplete packet (oct2 = %d).\n", r);
-		return -1;
-	}
+    r = recv(s, buf+4, 4, 0);
+    if (r != 4)
+    {
+        //log_format("received incomplete packet (oct2 = %d).\n", r);
+        return -1;
+    }
 
-	int pkt_size = *(int*)(buf);
-	int pkt_flags = *(int*)(buf+4);
-	
-	if((pkt_flags & 0x80100000) == 0x80100000)
-	{
-		//log_format("received special packet (size = %u, flags = %08X).\n", pkt_size, pkt_flags);
+    int pkt_size = *(int*)(buf);
+    int pkt_flags = *(int*)(buf+4);
+    
+    if ((pkt_flags & 0x80100000) == 0x80100000)
+    {
+        //log_format("received special packet (size = %u, flags = %08X).\n", pkt_size, pkt_flags);
 
-		uint8_t* rd = new uint8_t[pkt_size];
-		r = recv(s, (char*)rd, pkt_size, 0);
-		if(r != pkt_size)
-		{
-			delete[] rd;
-			return -1;
-		}
+        uint8_t* rd = new uint8_t[pkt_size];
+        r = recv(s, (char*)rd, pkt_size, 0);
+        if (r != pkt_size)
+        {
+            delete[] rd;
+            return -1;
+        }
 
-		Packet pack;
-		pack.Reset();
-		for(uint32_t i = 0; i < pkt_size; i++)
-			pack.WriteUInt8(rd[i]);
-		pack.Seek(0);
+        Packet pack;
+        pack.Reset();
+        for (uint32_t i = 0; i < pkt_size; i++)
+            pack.WriteUInt8(rd[i]);
+        pack.Seek(0);
 
-		delete[] rd;
-		
-		if(!Cl_ProcessServerPacket(pack))
-			return -1;
+        delete[] rd;
+        
+        if (!Cl_ProcessServerPacket(pack))
+            return -1;
 
-		// ok now make it look like we just received a 0x64 packet
-		*(uint32_t*)(buf) = 5;
-		*(uint32_t*)(buf+4) = 0x01000000;
-		*(uint8_t*)(buf+8) = 0x64 ^ key_20[0];
-		*(uint32_t*)(buf+9) = 1 ^ *(uint32_t*)(key_20+1);
-		return 8+5;
-	}
-	else
-	{
-		//log_format("received allods packet (size = %u, flags = %08X).\n", pkt_size, pkt_flags);
-		r = recv(s, buf+8, pkt_size, 0);
-		/*
-		for (int i = 0; i < pkt_size; i++)
-		{
-			log_format2("%02X ", (uint8_t)buf[8+i] ^ key_20[i%0x50]);
-		}
-		log_format("\n\n");*/
-		if(r != pkt_size)
-		{
-			recv_lastCustom = false;
-			recv_lastIncomplete = true;
-			recv_lastRemaining = pkt_size - r;
-		}
-		r += 8;
-	}
+        // ok now make it look like we just received a 0x64 packet
+        *(uint32_t*)(buf) = 5;
+        *(uint32_t*)(buf+4) = 0x01000000;
+        *(uint8_t*)(buf+8) = 0x64 ^ key_20[0];
+        *(uint32_t*)(buf+9) = 1 ^ *(uint32_t*)(key_20+1);
+        return 8+5;
+    }
+    else
+    {
+        //log_format("received allods packet (size = %u, flags = %08X).\n", pkt_size, pkt_flags);
+        r = recv(s, buf+8, pkt_size, 0);
+        /*
+        for (int i = 0; i < pkt_size; i++)
+        {
+            log_format2("%02X ", (uint8_t)buf[8+i] ^ key_20[i%0x50]);
+        }
+        log_format("\n\n");*/
+        if (r != pkt_size)
+        {
+            recv_lastCustom = false;
+            recv_lastIncomplete = true;
+            recv_lastRemaining = pkt_size - r;
+        }
+        r += 8;
+    }
 
-	return r;
+    return r;
 }
 
 void __declspec(naked) NETPROTO_fixRecv()
 {
-	__asm
-	{
+    __asm
+    {
 //40DB2C
-		mov		[ebp-0x11BF4], edx
+        mov        [ebp-0x11BF4], edx
 
-		mov		eax, [ebp-0x11BF4]
-		sub		eax, 3
-		mov		[ebp-0x11BF4], eax
-		mov		edx, 0x0040DB41
-		jmp		edx
+        mov        eax, [ebp-0x11BF4]
+        sub        eax, 3
+        mov        [ebp-0x11BF4], eax
+        mov        edx, 0x0040DB41
+        jmp        edx
 
 skip_packet:
-		mov		edx, 0x00417C03
-		jmp		edx
-	}
+        mov        edx, 0x00417C03
+        jmp        edx
+    }
 }
 
 //0 100 005122D1
@@ -336,30 +336,30 @@ char newNetBuffer[51200];
 
 void __declspec(naked) NETPROTO_enlargeBuffer()
 {
-	__asm
-	{
-		push	ebp
-		mov		ebp, esp
-		mov		eax, 0x201C
-		mov		edx, 0x0058D720
-		call	edx // __alloca_probe
-		mov		[ebp-0x2018], ecx
-		push	0
-		push	0xC800 // 51200
-		lea		eax, newNetBuffer
-		push	eax
-		mov		edx, 0x005122F2
-		jmp		edx
-	}
+    __asm
+    {
+        push    ebp
+        mov        ebp, esp
+        mov        eax, 0x201C
+        mov        edx, 0x0058D720
+        call    edx // __alloca_probe
+        mov        [ebp-0x2018], ecx
+        push    0
+        push    0xC800 // 51200
+        lea        eax, newNetBuffer
+        push    eax
+        mov        edx, 0x005122F2
+        jmp        edx
+    }
 }
 
 void __declspec(naked) NETPROTO_enlargeBuffer2()
 {
-	__asm
-	{
-		lea		eax, newNetBuffer
-		mov		[ebp-0x08], eax
-		mov		edx, 0x0051231D
-		jmp		edx
-	}
+    __asm
+    {
+        lea        eax, newNetBuffer
+        mov        [ebp-0x08], eax
+        mov        edx, 0x0051231D
+        jmp        edx
+    }
 }
