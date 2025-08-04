67ec7fdbd0be6d92ed5d47f034170df8ee2c4f96
ivan <cool.pepyaka@gmail.com>
2025-08-05 00:13:41 +0200

    Custom hat address can be provided in `-hat` command-line flag

--- network_hat_enter.cpp
+++ network_hat_enter.cpp
@@ -1,89 +1,88 @@
 #include <windows.h>
 #include <string>
 
-#define URL "hat.rom2.ru"
+#include "utils.h"
 
-char aHat[] = URL;
+const char* defaultHatURL = "hat.rom2.ru";
+std::string hat;
+char aHat[256];
 
-const char * aLogin;
-const char * aPassword;
-std::string cmdLine;
-std::string match;
-void _stdcall parseLogin(){
-    
-    
-    int _start;
-    int _end;
-    cmdLine = GetCommandLineA();
-    if((_start = cmdLine.find("-login")) != std::string::npos) {
-        if((_end = cmdLine.find(" ",_start+7)) != std::string::npos){
-            match = cmdLine.substr(_start+7, _end - _start - 7);
-        }else{
-            match = cmdLine.substr(_start+7);
-        }
-        aLogin = match.c_str();
-        
-        if((_start = cmdLine.find("-password")) != std::string::npos) {
-           if((_end = cmdLine.find(" ",_start+10)) != std::string::npos){
-               match = cmdLine.substr(_start+10, _end - _start - 10);
-           }else{
-               match = cmdLine.substr(_start+10);
-           }
-           aPassword = match.c_str();
+std::string login;
+const char* aLogin;
+std::string password;
+const char* aPassword;
 
-        }else{
-            //get password from registry
-            
-            //__asm
-            //{
-            //mov		edx, 0x0043F7ED
-            //jmp		edx
-            //}
-            
+void ParseLogin() {
+    auto args = ParseCommandLine(GetCommandLineA());
+
+    for (size_t i = 0; i+1 < args.size(); ++i) {
+        if (args[i] == "-login") {
+            login = args[i+1];
+            aLogin = login.c_str();
+        } else if (args[i] == "-password") {
+            password = args[i+1];
+            aPassword = password.c_str();
         }
-        
-    }else{
-        //get login and password defaults from registry
-        /*
-        __asm
-        {
-        mov		edx, 0x0043F755
-        jmp		edx
-        }
-        */
     }
 }
-void __declspec(naked) HATENT_login_text()
-{
 
-    parseLogin();
+void __declspec(naked) HATENT_login_text() {
+    ParseLogin();
     
     __asm
     {
+        mov  eax, aLogin
+        push eax
+        mov  ecx, [ebp-0x124]
+        add  ecx, 0x0C
+        mov  edx, 0x005AB9E0
+        call edx
 
-  mov  eax, aLogin
-  push eax
-  mov  ecx, [ebp-0x124]
-  add  ecx, 0x0C
-  mov  edx, 0x005AB9E0
-  call edx
+        mov  eax, aPassword
+        push eax
+        mov  ecx, [ebp-0x124]
+        add  ecx, 0x10
+        mov  edx, 0x005AB9E0
+        call edx
 
-  mov  eax, aPassword
-  push eax
-  mov  ecx, [ebp-0x124]
-  add  ecx, 0x10
-  mov  edx, 0x005AB9E0
-  call edx
-
-  //mov  edx, 0x0043F7ED //to password
-  mov  edx, 0x0043F885
-  jmp  edx
+        //mov  edx, 0x0043F7ED //to password
+        mov  edx, 0x0043F885
+        jmp  edx
     }
-
 }
 
-void __declspec(naked) HATENT_url_text()
-{
+void ParseHatAddress() {
+    if (!hat.empty()) {
+        return;
+    }
+
+    auto args = ParseCommandLine(GetCommandLineA());
+
+    for (size_t i = 0; i+1 < args.size(); ++i) {
+        if (args[i] == "-hat") {
+            hat = args[i+1];
+            log_format("Using hat address: '%s'\n", hat.c_str());
+        }
+    }
+
+    if (hat.empty()) {
+        hat = defaultHatURL;
+    }
+
+    if (hat.length() > sizeof(aHat)) {
+        hat = hat.substr(0, sizeof(aHat)-1);
+        log_format("Hat address cut to: '%s'. Have a shorter hat address :P\n", hat.c_str());
+    }
+
+    for (size_t i = 0; i < hat.length(); ++i) {
+        aHat[i] = hat[i];
+    }
+    aHat[hat.length()] = '\0';
+}
+
+void __declspec(naked) HATENT_url_text() {
+    ParseHatAddress();
+
     __asm
     {
         mov		eax, offset aHat
@@ -109,8 +108,9 @@ void __declspec(naked) HATENT_url_text()
     }
 }
 
-void __declspec(naked) HATENT_url_connect()
-{
+void __declspec(naked) HATENT_url_connect() {
+    ParseHatAddress();
+
     __asm
     {
         add		esp, 4
