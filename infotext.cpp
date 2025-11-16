81bca2e6d6eb20754d9b2a87d4ef48372e31a7ab
ivan <cool.pepyaka@gmail.com>
2025-11-16 23:45:08 +0100

    Fix left sprite position
    If the height of the window is not divisible by 32, there's a black line
    at the bottom of the screen, where map tiles are not drawn. The bottom
    of the rectangle that the sprite uses is at the top of that line, not at
    the bottom.

--- infotext.cpp
+++ infotext.cpp
@@ -1,9 +1,21 @@
 #include <windows.h>
 
+#include "zxmgr.h"
+
 int __fastcall InfoPanelLeftSpriteY(RECT* at) {
+    const int sprite_height = 242;
+
     // Vanilla logic: draw the left part of the info panel at `y = at->top + 0x1e0 + 46`.
     // However, the right part (with all the text) is drawn at the bottom of the screen. Let's move the left part down.
-    return at->bottom - 242; // 242 is the sprite height.
+    auto hwnd = zxmgr::GetHWND();
+    RECT rect;
+    if (GetWindowRect(hwnd, &rect)) {
+        return rect.bottom - sprite_height;
+    }
+
+    // Can't get window size? Draw at the bottom of whatever rectangle we have. Shouldn't happen.
+    // 242 is the sprite height.
+    return at->bottom - sprite_height;
 }
 
 // Address: 0040a939
